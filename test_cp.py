911aa98b107825cb86416025d6e6366772965be7
Marie Roald <roald.marie@gmail.com>
2018-10-11 16:04:02 +0200

    Added more tests

--- test_cp.py
+++ test_cp.py
@@ -8,8 +8,6 @@ from scipy import optimize
 
 """
 TODO: (tests)
-- Test that the loss is zero for exact decomposition
-- Test that the gradient of loss is zero for exact decomposition
 - 
 """
 
@@ -31,6 +29,32 @@ def test_scipy_check_grad():
     args = (rank, sizes, tensor)
     check_grad_error = optimize.check_grad(cp._cp_loss_scipy, cp._cp_grad_scipy, flattened_factors, *args)
     assert check_grad_error==approx(0, abs=1e-5)
+
+
+def test_loss_is_zero_for_exact_decomposition():
+    sizes = [5, 3, 10]
+    rank = 2
+
+    factors = _random_factors(sizes, rank)
+    tensor = base.ktensor(*tuple(factors))
+
+    assert cp.cp_loss(factors, tensor) == approx(0)
+
+def test_grad_is_zero_for_exact_decomposition():
+    sizes = [5, 3, 10]
+    rank = 2
+
+    factors = _random_factors(sizes, rank)
+    tensor = base.ktensor(*tuple(factors))
+
+    grads = cp.cp_grad(factors, tensor)
+    for grad in grads:
+        assert np.all(grad<1e-10)
+
+
+    
+
+    
 
 
 
