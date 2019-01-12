a1e212e48e279fdaeb07c48cb9c7352238e390a1
DESKTOP-5DCVUHT\eiram <roald.marie@gmail.com>
2019-01-12 14:11:53 +0100

    Updated so matrix khatri rao is better, til will probably lead to parafac2 conflict...

--- base.py
+++ base.py
@@ -48,30 +48,23 @@ def khatri_rao(*factors, skip=None):
     return product
 
 def matrix_khatri_rao_product(X, factors, mode):
+    assert len(X.shape) == len(factors)
+    if len(factors) == 3:
+        return _mttkrp3(X, factors, mode)
+
     return unfold(X, mode) @ khatri_rao(*tuple(factors), skip=mode)
         
-def mttkrp(tensor, matrices, mode):
-    krp = khatri_rao(*matrices, skip=mode)
-    shape = tensor.shape
-    newshape = (*shape[:mode+1], -1)
-    unfolded_tensor = tensor.reshape(newshape)
-
-    num_for_loops = mode
-    def num_to_idx(num):
-        idx = []
-        for s in newshape[-3::-1]:
-            idx.append(num%s)
-            num //= s
-        return tuple(idx[::-1])
-
-    block_size = unfolded_tensor.shape[-1]
-    num_rows = unfolded_tensor.shape[-2]
-    num_cols = krp.shape[-1]
-    product = np.zeros((num_rows, num_cols))
-    for i in range(np.prod(shape[:mode]).astype(np.int16)):
-        product += unfolded_tensor[num_to_idx(i)]@krp[i*block_size:(i+1)*block_size]
-
-    return product
+def _mttkrp3(X, factors, mode):
+    if mode == 0:
+        return X.reshape(X.shape[0], -1) @ khatri_rao(*tuple(factors), skip=mode)
+    elif mode == 1:
+        return _mttkrp_mid(X, factors)
+    elif mode == 2 or mode == -1:
+        return np.moveaxis(X, -1, 0).reshape(X.shape[-1], -1) @ khatri_rao(*tuple(factors), skip=mode)
+    
+def _mttkrp_mid(tensor, matrices):
+    krp = khatri_rao(*matrices, skip=1)
+    return _mttkrp_mid_with_krp(tensor, krp)
 
 def unfold(A, n):
     """Unfold tensor to matricizied form.
