94dd65c9f5f66faf1392b670373df9b0911666bc
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2019-03-23 14:06:56 +0100

    Updated docstrings for _init_fit, fit and continue_fit in BaseDecomposer

--- pytensor/decomposition/base_decomposer.py
+++ pytensor/decomposition/base_decomposer.py
@@ -56,6 +56,18 @@ class BaseDecomposer(ABC):
         pass
 
     def _init_fit(self, X, max_its=None, initial_decomposition=None):
+        """Prepare model for fitting by initialising and setting target and max_its.
+
+        Arguments
+        ---------
+        X : np.ndarray
+            The tensor to fit the model to
+        max_its : int (optional)
+            If set, then this will override the class's max_its.
+        initial_decomposition : BaseDecomposedTensor (optional)
+            None (default) or a BaseDemposedTensor object containig the 
+            initial decomposition. If class's init is not 'precomputed' it is ignored.
+        """
         self.init_components(initial_decomposition=initial_decomposition)
         self.set_target(X)
         if max_its is not None:
@@ -66,10 +78,10 @@ class BaseDecomposer(ABC):
         
         Precomputed components must be specified if init method is 'precomputed'.
 
-        Arguments:
-        ----------
+        Arguments
+        ---------
         X : np.ndarray
-            The tensor to fit
+            The tensor to fit the model to
         y : None
             Ignored, included to follow sklearn standards.
         max_its : int (optional)
@@ -82,6 +94,13 @@ class BaseDecomposer(ABC):
         self._fit()
     
     def continue_fit(self, max_its=None):
+        """Continue training an allready fitted model.
+
+        Arguments
+        ---------
+        max_its : int (optional)
+            If set, then this will override the class's max_its. s
+        """
         if max_its is not None:
             self.max_its = max_its
         self._fit()
