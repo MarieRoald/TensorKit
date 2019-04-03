dc6f14a0884e251fde9658465ef994ba062a00e4
Marie Roald <roald.marie@gmail.com>
2019-04-03 07:16:03 +0200

    Added tests for fms

--- pytensor/decomposition/tests/test_cmtf.py
+++ pytensor/decomposition/tests/test_cmtf.py
@@ -11,6 +11,8 @@ from .. import cmtf
 
 np.random.seed(0)
 
+#TODO: test coupling on different modes and more than one mode
+
 class TestCMTFALS:
     @pytest.fixture
     def rank4_kruskal_tensor(self):
@@ -31,13 +33,20 @@ class TestCMTFALS:
         A = rank4_kruskal_tensor.factor_matrices[0]
         Y = A @ V.T
 
-        cmtf_decomposer = cmtf.CMTF_ALS(4, max_its=1000, convergence_tol=1e-10, print_frequency=1)
+        cmtf_decomposer = cmtf.CMTF_ALS(4, max_its=2000, convergence_tol=1e-10, print_frequency=100)
 
         estimated_ktensor, estimated_Y_factors = cmtf_decomposer.fit_transform(X, [Y], [0])
         estimated_X = estimated_ktensor.construct_tensor()
-        estimated_A, estimated_V, estimated_weights = estimated_Y_factors[0]
+        estimated_A, estimated_V = estimated_Y_factors[0]
 
-        estimated_Y = estimated_A @ (estimated_weights* estimated_V).T
+        estimated_Y = estimated_A @ estimated_V.T
         assert np.allclose(X, estimated_X)
         assert np.allclose(Y, estimated_Y)
 
+        assert metrics.factor_match_score(
+            [A, V], [estimated_A, estimated_V], weight_penalty=False
+        )[0] > 1-1e-9
+
+        assert metrics.factor_match_score(
+            rank4_kruskal_tensor.factor_matrices, estimated_ktensor.factor_matrices, weight_penalty=False
+        )[0] > 1-1e-9
