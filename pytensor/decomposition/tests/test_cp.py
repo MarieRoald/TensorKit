985af302ba0f37801659309a879f3668c9bc458d
Marie Roald <roald.marie@gmail.com>
2019-04-09 18:07:14 +0200

    added nonnegative test

--- pytensor/decomposition/tests/test_cp.py
+++ pytensor/decomposition/tests/test_cp.py
@@ -17,17 +17,37 @@ class TestCPALS:
         ktensor = base.KruskalTensor.random_init((30, 40, 50), rank=4)
         ktensor.normalize_components()
         return ktensor
+
+    @pytest.fixture
+    def nonnegative_rank4_kruskal_tensor(self):
+        ktensor = base.KruskalTensor.random_init((30, 40, 50), rank=4)
+        ktensor.normalize_components()
+        for i, fm in enumerate(ktensor.factor_matrices):
+            ktensor.factor_matrices[i][...] = np.abs(fm)
+
+        return ktensor
     
     def test_rank4_decomposition(self, rank4_kruskal_tensor):
         X = rank4_kruskal_tensor.construct_tensor()
-        cp_als = cp.CP_ALS(4, max_its=10000, convergence_tol=1e-10)
+        cp_als = cp.CP_ALS(4, max_its=1000, convergence_tol=1e-10)
         estimated_ktensor = cp_als.fit_transform(X)
         estimated_ktensor.normalize_components()
 
         assert np.allclose(X, estimated_ktensor.construct_tensor())
         assert metrics.factor_match_score(
             rank4_kruskal_tensor.factor_matrices, estimated_ktensor.factor_matrices
-        )[0] > 1-1e-10
+        )[0] > 1-1e-7
+
+    def test_rank4_nonnegative_decomposition(self, nonnegative_rank4_kruskal_tensor):
+        X = nonnegative_rank4_kruskal_tensor.construct_tensor()
+        cp_als = cp.CP_ALS(4, max_its=1000, convergence_tol=1e-10, non_negativity_constraints=[True]*3)
+        estimated_ktensor = cp_als.fit_transform(X)
+        estimated_ktensor.normalize_components()
+
+        assert np.allclose(X, estimated_ktensor.construct_tensor())
+        assert metrics.factor_match_score(
+            nonnegative_rank4_kruskal_tensor.factor_matrices, estimated_ktensor.factor_matrices
+        )[0] > 1-1e-7
     
     def test_store_and_load_from_checkpoint(self, rank4_kruskal_tensor):
         max_its = 20
