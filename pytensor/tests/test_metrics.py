5ad2fb7a467d6455cb22989b63f9282602269af9
Marie Roald <roald.marie@gmail.com>
2019-07-18 17:13:26 +0200

    Changed whitespace

--- pytensor/tests/test_metrics.py
+++ pytensor/tests/test_metrics.py
@@ -9,8 +9,8 @@ class TestCoreConsistency:
     @pytest.fixture
     def random_parafac2_tensor(self):
         return Parafac2Tensor.random_init((10, [20]*30, 30), 5)
+
     def test_parafac2_core_consistency_perfect_decomposition(self, random_parafac2_tensor):
-        
         X = random_parafac2_tensor.construct_slices()
         P = random_parafac2_tensor.projection_matrices
         A = random_parafac2_tensor.A 
@@ -18,6 +18,5 @@ class TestCoreConsistency:
         C = random_parafac2_tensor.C
         
         cc = np.asscalar(metrics.core_consistency_parafac2(X, P, A, B, C))
-
         assert abs(cc-100) < 1e-10
         
