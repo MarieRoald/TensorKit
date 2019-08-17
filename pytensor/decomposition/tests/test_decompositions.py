Added fix for the sign indeterminacy problem of the CP model

pytensor/decomposition/tests/test_decompositions.py
@@ -7,9 +7,9 @@ import numpy as np
 class TestKruskalTensor:
     @pytest.fixture
     def random_3mode_ktensor(self):
-        A = np.random.randn(30, 3)
-        B = np.random.randn(40, 3)
-        C = np.random.randn(50, 3)
+        A = np.random.randn(30, 4)
+        B = np.random.randn(40, 4)
+        C = np.random.randn(50, 4)
 
         return decompositions.KruskalTensor([A, B, C])
     
@@ -44,9 +44,9 @@ class TestKruskalTensor:
             decompositions.KruskalTensor([A, B, C])
     
     def test_correct_size_of_tensor(self):
-        A = np.random.randn(30, 3)
-        B = np.random.randn(40, 3)
-        C = np.random.randn(50, 3)
+        A = np.random.randn(30, 4)
+        B = np.random.randn(40, 4)
+        C = np.random.randn(50, 4)
 
         ktensor = decompositions.KruskalTensor([A, B, C])
         assert ktensor.construct_tensor().shape == (30, 40, 50)
@@ -71,6 +71,36 @@ class TestKruskalTensor:
             for j, vector in enumerate(matrix):
                 for k, element in enumerate(vector):
                     assert abs(element - np.sum(A[i, :]*B[j, :]*C[k, :])) < 1e-8
+    
+    def test_sign_flip_yields_consistent_decomposition(self, random_3mode_ktensor):
+        X = random_3mode_ktensor.construct_tensor()
+        X += np.random.standard_normal(X.shape)*np.linalg.norm(X)*0.1
+        signs = random_3mode_ktensor.get_signs(X)
+
+        for rank in range(random_3mode_ktensor.rank):
+            sign_product = 1
+            for mode in range(3):
+                sign_product *= signs[mode][rank]
+            assert sign_product == 1
+    
+    def test_sign_flip_yields_same_sign_as_sign_score(self, random_3mode_ktensor):
+        X = random_3mode_ktensor.construct_tensor()
+        X += np.random.standard_normal(X.shape)*np.linalg.norm(X)*0.1
+        signs = random_3mode_ktensor.get_signs(X)
+        sign_scores = random_3mode_ktensor.get_sign_scores(X)
+
+        for rank in range(random_3mode_ktensor.rank):
+            sign_product = 1
+            num_equal_signs = 0
+            for mode in range(3):
+                sign_product *= np.sign(sign_scores[mode][rank])
+                num_equal_signs += np.sign(sign_scores[mode][rank]) == signs[mode][rank]
+            
+            if sign_product == -1:
+                assert num_equal_signs == 2
+            else:
+                assert num_equal_signs == 3
+
     
 
 class TestEvolvingTensor:

pytensor/utils.py
@@ -157,24 +157,42 @@ def prepare_for_comparison(factors):
     return normalized_factors, signs, norms
 
 
-def get_signs(X, factor_matrix, data_driven=True):
-    # TODO: hva om den er null?
-    if not data_driven:
-        return np.sign(factor_matrix.sum(axis=0))
+def get_signs(factor_matrix, X):
+    """Find the correct signs of the factor matrix.
+    If data matrix is not passed, then the sign is set so the factor mean is positive.
+    If the data matrix is passed, then the sign is set so the factors are positively
+    correlated with the data matrix.
 
-    S = np.linalg.lstsq(factor_matrix, X)[0]
-    signs = np.zeros(factor_matrix.shape[1])
-    for r in range(factor_matrix.shape[1]):
-        signs[r] = np.sign(np.sum(S[r]**2 * np.sign(S[r]))).astype(factor_matrix.dtype)
-        # signs[r] = np.sign(np.sum(S[r])).astype(factor_matrix.dtype)  <- This did not work either
+    Arguments
+    ---------
+    factor_matrix : np.ndarray
+        Factor matrix with unknown sign
+    X : np.ndarray
+        Data matrix
+
+    Returns
+    -------
+    sign : int
+    sign_weight : float
+    """
+    if X is None:
+        sign_weight = factor_matrix.sum(axis=0)
+        sign_weight[sign_weight == 0] = 1
+        return np.sign(sign_weight), sign_weight
+
+    X_described_by_factors = np.linalg.lstsq(factor_matrix, X)[0]
+    sign_weight = np.sum(
+        np.sign(X_described_by_factors) * X_described_by_factors**2,
+        axis=1
+    )
+
+    return np.sign(sign_weight), sign_weight
     
-    return signs
-
 def signfix_evolving_factors(data_tensor, evolving_factor, evolve_over_factor, data_driven=True):
     fixed_evolving_factor = evolving_factor.copy()
     fixed_evolve_over_factor = evolve_over_factor.copy()
     for k in range(len(evolving_factor)):
-        signs = get_signs(data_tensor[k], evolving_factor[k], data_driven=data_driven)
+        signs = get_signs(data_tensor[k], evolving_factor[k], data_driven=data_driven)[0]
         
         fixed_evolving_factor[k] *= signs
         fixed_evolve_over_factor[k] *= signs
@@ -183,12 +201,11 @@ def signfix_evolving_factors(data_tensor, evolving_factor, evolve_over_factor, d
 
 def signfix_normal_factors(data_tensor, fixing_factors, flipping_factors, data_driven=True):
     unfolded_data_tensor = data_tensor.reshape(data_tensor.shape[0], -1)
-    signs = get_signs(unfolded_data_tensor, fixing_factors, data_driven)
+    signs = get_signs(unfolded_data_tensor, fixing_factors, data_driven)[0]
     
     return fixing_factors*signs[np.newaxis], flipping_factors*signs[np.newaxis]
 
 def fix_signs_evolving_tensor(evolving_tensor, data_tensor):
-
     evolving_factor = evolving_tensor.B
     evolve_over_factor = evolving_tensor.C
 