0a22901f9894be29727349a007b6f0b4f4580774
Marie Roald <roald.marie@gmail.com>
2019-08-17 19:02:43 +0200

    pip is apparently case insensitive

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="tensorkit",
+    name="TensorKit",
     version="0.0.001a",
     packages=find_packages("src"),
     package_dir={"": "src"}
