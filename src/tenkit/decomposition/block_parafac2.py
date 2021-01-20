f2882871f40bd0a702074b8de55479fe6b736dcf
Marie Roald <roald.marie@gmail.com>
2021-01-20 18:57:42 +0100

    Updated file

--- src/tenkit/decomposition/block_parafac2.py
+++ src/tenkit/decomposition/block_parafac2.py
@@ -1,4 +1,3 @@
-from block_tenkit_admm import *
 from warnings import warn
 
-warn("This file is moved to a separate project, `block_tenkit_admm`, but it is kept here in a deprecation period.")
+warn("This file is moved to a separate project, `bcd_tenkit`")
